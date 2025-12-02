13342c59111075056ef5fb20784c441602b60e3d
Bibin Thomas <bibinthomas4897@gmail.com>
2025-12-02 19:54:11 +0100

    create all pages

---
+++ script.js
@@ -0,0 +1,13 @@
+fetch(".html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+
+    // Optional: highlight current page
+    const currentPage = window.location.pathname.split("/").pop();
+    document.querySelectorAll(".nav-item").forEach(link => {
+      if (link.getAttribute("href") === currentPage) {
+        link.classList.add("active"); // you can style .active in CSS
+      }
+    });
+  });
