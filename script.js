98a52cf5d30099b4dd38a0954a656936f5148c83
Bibin Thomas <bibinthomas4897@gmail.com>
2025-12-15 01:07:45 +0100

    update

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-fetch("header.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("header-section").innerHTML = data;
 
-    // Optional: highlight current page
-    const currentPage = window.location.pathname.split("/").pop();
-    document.querySelectorAll(".nav-item").forEach(link => {
-      if (link.getAttribute("href") === currentPage) {
-        link.classList.add("active"); // you can style .active in CSS
-      }
-    });
-  });
